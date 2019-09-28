f109f5b4b0c63a12d60a7e1145f26c01ec2effa8
cadeon <cadeon924@gmail.com>
2019-09-28 16:18:22 -0400

    Spin Spin Stations

---
+++ Unity/spacejam/Assets/Scripts/SpinStations.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinStations : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate all the stations
+        transform.RotateAround(Vector3.zero, Vector3.forward, 10 * Time.deltaTime);
+
+        }
+}
