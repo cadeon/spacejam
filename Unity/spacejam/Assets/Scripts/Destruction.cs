9a4750910a66fb8cb5a7a18fffb4eb5e8c939f38
emilio-alban <amilo324@gmail.com>
2019-09-28 16:02:00 -0400

    Added Debris
    Collisions now spawn debris

--- Unity/spacejam/Assets/Scripts/Destruction.cs
+++ Unity/spacejam/Assets/Scripts/Destruction.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Destruction : MonoBehaviour
 {
     public int health = 1;
+    public GameObject debrisObject;
+    public int numberOfDebris;
+    bool isQuitting = false;
+    float debrisInitialDistance = 1.0f;
+    float debrisInitialVelocity = 40.0f;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -21,9 +26,25 @@ public class Destruction : MonoBehaviour
         }
     }
 
+    void OnApplicationQuit()
+    {
+        isQuitting = true;
+    }
+
     private void OnDestroy()
     {
         // TODO: Spawn debris 
+        if (!isQuitting)
+        {
+            for (int i = 0; i < numberOfDebris; i++)
+            {
+                GameObject debris = Instantiate(debrisObject);
+                Vector2 randomVector = Random.onUnitSphere;
+                randomVector = randomVector.normalized;
+                debris.transform.position = transform.position + (Vector3)randomVector*debrisInitialDistance;
+                debris.GetComponent<Rigidbody2D>().AddForce(gameObject.GetComponent<Rigidbody2D>().velocity + randomVector*debrisInitialVelocity);
+            }
+        }
     }
 
 }
