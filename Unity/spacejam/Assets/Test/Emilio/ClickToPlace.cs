bd0b16420fba3665ee166b143762354821f31aab
emilio-alban <amilo324@gmail.com>
2019-09-28 10:23:57 -0400

    Added click-to-spawn
    You can now click to spawn a satellite by left-clicking
    Also fixed some problems with unupdated prefabs

--- Unity/spacejam/Assets/Test/Emilio/ClickToPlace.cs
+++ Unity/spacejam/Assets/Test/Emilio/ClickToPlace.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ClickToPlace : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject objectToSpawn;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 clickLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            print("Mouse Clicked. Coordinates: " + clickLocation);
+            GameObject spawnedObject = Instantiate(objectToSpawn);
+            spawnedObject.transform.position = clickLocation;
+        }
     }
 }
