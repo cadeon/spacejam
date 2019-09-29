a3476ea663f55364a168bc0ed046a342e49af4a2
emilio-alban <amilo324@gmail.com>
2019-09-28 21:56:42 -0400

    Input Revamp
     - Moved player inputs to the SpaceStation prefab. Will be useful when we get multiple controllers.
     - Disabled player inputs on the PlayerController prefab (see above)
     - Added labels to the SpaceStation prefabs.
     - Added the ability to switch which type of object to fire. Right now you can switch between Satellites and Debris with the Y button.
     - Fixed debug spam in OrbitalPhysics.cs

---
+++ Unity/spacejam/Assets/PlayerControlScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    public GameObject[] satellites;
+    public int selected = 0;
+    //public GameObject objectToSpawn;
+    GameObject spawnedObject;
+    public float launchPower;
+    public Transform frontEnd;
+    public Transform backEnd;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Switch Satellites 
+        if (Input.GetButtonDown("Select Satellite"))
+        {
+            if (selected < satellites.Length - 1)
+                selected++;
+            else
+                selected = 0;
+            Debug.Log("Selected Satellite: " + selected);
+        }
+
+        // Hold to charge & launch
+        if (Input.GetAxis("RightTrigger") != 0)
+        {
+            launchPower += 250 * Time.deltaTime;
+        }
+        else if(launchPower > 0.0f)
+        {
+            spawnedObject = Instantiate(satellites[selected], transform.position, Quaternion.identity);
+            spawnedObject.GetComponent<Rigidbody2D>().AddForce((transform.position - backEnd.position) * launchPower);
+            launchPower = 0.0f;
+        }
+
+    }
+}
