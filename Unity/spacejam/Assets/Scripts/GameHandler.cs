3ba64d08cdce685f234b4c4de72c4bbcb9bff5ef
Mike Bianchini <mbianchini@dignitastechnologies.com>
2019-09-29 11:33:52 -0400

    FIrst pass fo health bar from tutorial and now making it pretty

---
+++ Unity/spacejam/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    [SerializeField] private HealthBar healthBar;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        healthBar.SetSize(.4f);
+    }
+
+
+}
